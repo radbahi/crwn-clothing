45581f6ecbb445b19944ccfb7b615cfb441f79a3
Radouane Bahi <radbahi@gmail.com>
2020-12-27 13:01:02 -0500

    splitting into components

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,9 @@
+import Directory from '../../components/directory/Directory'
+import './Homepage.scss'
+
+//not styled the same for some reason
+const Homepage = () => {
+  return <Directory />
+}
+
+export default Homepage
