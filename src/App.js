485884835a4cc9e1d1f4ff5a27ed49a2f9e4b755
Radouane Bahi <radbahi@gmail.com>
2021-01-03 11:49:38 -0500

    setup subscription and memory leak prevention

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import { Switch, Route } from 'react-router-dom'
+import { auth } from './firebase/firebase.utils'
 import './App.css'
 import HomePage from './pages/homepage/Homepage.js'
 import ShopPage from './pages/shop/Shop.js'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/SignInAndSignUp'
 import Header from './components/header/Header'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  )
+class App extends React.Component() {
+  state = { currentUser: null }
+
+  // set up an unsubscribe to prevent memory leaks when app component unmounts
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    // the user parameter below is the user auth state of firebase which we then set to our app state on mount. also provides session persistence
+    // https://www.udemy.com/course/complete-react-developer-zero-to-mastery/learn/lecture/15082482#questions
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user })
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App
