3119567990094dd92722c5facc2a34746c0d68c9
Radouane Bahi <radbahi@gmail.com>
2021-01-12 13:47:57 -0500

    added itemcount value to state

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -2,11 +2,12 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import { toggleCartHidden } from '../../redux/cart/cart-actions'
 import { connect } from 'react-redux'
 import './CartIcon.scss'
+import cartItem from '../cart-item/CartItem'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 )
 
@@ -14,4 +15,12 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+// below is a selector. adds up items.
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  ),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
