ff619eb60e1b2c3d6fb7df3878c0cb7cdc7cc77a
Radouane Bahi <radbahi@gmail.com>
2021-01-18 14:18:52 -0500

    url params matched to id with string as key

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -1,5 +1,7 @@
 import './Collection.scss'
 import CollectionItem from '../../components/collection-item/CollectionItem'
+import { connect } from 'react-redux'
+import { selectCollection } from '../../redux/shop/shop-selectors'
 
 const CollectionPage = ({ match }) => {
   return (
@@ -8,5 +10,9 @@ const CollectionPage = ({ match }) => {
     </div>
   )
 }
+//https://www.udemy.com/course/complete-react-developer-zero-to-mastery/learn/lecture/15175872#questions
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+})
 
-export default CollectionPage
+export default connect(mapStateToProps)(CollectionPage)
--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -1,8 +1,27 @@
 import { createSelector } from 'reselect'
+import memoize from 'lodash.memoize' //https://www.udemy.com/course/complete-react-developer-zero-to-mastery/learn/lecture/20796198#questions
+
+const COLLECTIONS_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+}
 
 const selectShop = (state) => state.shop
 
+//get all collections
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+)
+
+//find single collection
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector([selectCollections], (collections) =>
+    collections.find(
+      (collection) => collection.id === COLLECTIONS_ID_MAP[collectionUrlParam]
+    )
+  )
 )
