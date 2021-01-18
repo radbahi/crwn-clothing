885c20477081bb625cafcbc37b8196ee5cbc6495
Radouane Bahi <radbahi@gmail.com>
2021-01-18 14:03:02 -0500

    separated concerns of collection and shop

---
+++ src/pages/collection/Collection.js
@@ -0,0 +1,12 @@
+import './Collection.scss'
+import CollectionItem from '../../components/collection-item/CollectionItem'
+
+const CollectionPage = ({ match }) => {
+  return (
+    <div className='collection-page'>
+      <h3>Collection pAGE</h3>
+    </div>
+  )
+}
+
+export default CollectionPage
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,8 +1,13 @@
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview'
+import CollectionPage from '../collection/Collection'
+import { Route } from 'react-router-dom'
 
-const ShopPage = ({ collections }) => (
+//https://www.udemy.com/course/complete-react-developer-zero-to-mastery/learn/lecture/15175184#questions
+
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 )
 
