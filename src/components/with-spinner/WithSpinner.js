5b6842061b95b0ea53d801cfd2ef6ee65180de19
Radouane Bahi <radbahi@gmail.com>
2021-03-03 09:20:29 -0500

    removed extra wrappedcomponent function from spinner

--- src/components/with-spinner/WithSpinner.js
+++ src/components/with-spinner/WithSpinner.js
@@ -2,7 +2,7 @@ import { SpinnerContainer, SpinnerOverlay } from './WithSpinner.styles'
 
 //https://www.udemy.com/course/complete-react-developer-zero-to-mastery/learn/lecture/15190698#questions
 const WithSpinner = (WrappedComponent) => {
-  const Spinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
     return isLoading ? (
       <SpinnerOverlay>
         <SpinnerContainer />
