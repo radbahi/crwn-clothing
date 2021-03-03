fe117e4561a2b0953039e855b6bda0722b4c4147
Radouane Bahi <radbahi@gmail.com>
2021-03-03 10:06:34 -0500

    changing data retrieval to rest call

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -26,7 +26,8 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props
     const collectionRef = firestore.collection('collections')
     //whenever collectionRef updates or code gets run for the first time, below will send us the array
-    collectionRef.onSnapshot(async (snapshot) => {
+
+    collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap)
       this.setState({ loading: false })
